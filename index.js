da6d11676d08a0e9fa41717f1b3aa724f5a7fc46
Matthew Parris <mfrogparris@gmail.com>
2019-09-18 21:38:19 -0500

    Add a centralized file for the loggers
    - All debug loggers should be declared in logger.js
    
    - Always import the loggers from this file

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const log = require('./loggers').log;
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
+});
---
+++ loggers.js
@@ -0,0 +1,4 @@
+const debug = require('debug');
+const log = debug('pswd:log');
+
+module.exports = { log };
