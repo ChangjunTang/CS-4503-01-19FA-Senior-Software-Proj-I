6de8c6267eeedfafe70278be8255df25543fdca0
Matthew Parris <mfrogparris@gmail.com>
2019-09-18 21:46:42 -0500

    Fix naming of static folder
    - Folder name was on .gitignore
    
    - Changed the name of the folder so that it is no longer being ignored

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('static'));
 
 app.listen(port, function () {
     console.log(`Listening on port ${port}`);
