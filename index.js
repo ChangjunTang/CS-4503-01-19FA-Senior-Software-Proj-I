98c980d1554bab0e78e5a40e8a256e81fd3f0121
Matthew Parris <mfrogparris@gmail.com>
2019-09-20 21:00:56 -0500

    Log all requests
    - Logs the ip address, request type, url, and cookies for all requests
    
    - Useful for debugging and auditing

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Logs all requests to the console
+app.use(function (req, res, next) {
+    log(`${req.ip} ${req.method} ${req.originalUrl} ${req.cookies || 'No cookies'}`);
+    next();
+});
+
 app.use(express.static('static'));
 
 app.listen(port, function () {
