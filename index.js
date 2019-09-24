9b11c1162a1ea1d95c73736f4f0fedb4af9821e5
Alexa <alb146@utulsa.edu>
2019-09-24 00:29:18 -0500

    firebase config info added

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const session = require('express-session');
 const app = express();
 const port = 3000;
 
+var config = {
+    apiKey: "AIzaSyDcPUV7GN5aLzWnUrst7-pU0WHNxta0Qbs",
+    authDomain: "jjbp-bot.firebaseapp.com",
+    databaseURL: "https://jjbp-bot.firebaseio.com",
+    projectId: "jjbp-bot",
+    storageBucket: "jjbp-bot.appspot.com",
+    messagingSenderId: "757073654109"
+};
+// initialize the firebase app
+var firebase = require("firebase");
+firebase.initializeApp(config);
+// save the firebase database to a variable
+var database = firebase.database();
+
 // Logs all requests to the console
 app.use(function (req, res, next) {
     log(`${req.ip} ${req.method} ${req.originalUrl}`);
