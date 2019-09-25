cc1ce632ac1f5c552d5c872df126eba832ece274
ChangjunTang <54614068+ChangjunTang@users.noreply.github.com>
2019-09-25 13:32:27 -0500

    update front end js with apiKey and etc.

--- JJBP-BOT.js
+++ JJBP-BOT.js
@@ -8,6 +8,16 @@ window.onclick = function(event) {
     }
 }
 
+var config = {
+    apiKey: "AIzaSyDcPUV7GN5aLzWnUrst7-pU0WHNxta0Qbs",
+    authDomain: "jjbp-bot.firebaseapp.com",
+    databaseURL: "https://jjbp-bot.firebaseio.com",
+    projectId: "jjbp-bot",
+    storageBucket: "jjbp-bot.appspot.com",
+    messagingSenderId: "757073654109"
+};
+firebase.initializeApp(config);
+
 /**
  * Handles the sign in button press.
  */
