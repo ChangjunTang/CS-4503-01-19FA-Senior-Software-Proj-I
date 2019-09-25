37d374bfcf9a6e78e9cf7dd967d5a76bc931914b
Matthew Parris <mfrogparris@gmail.com>
2019-09-25 17:31:59 -0500

    Add login error message to page itself
    - Used to redirect to a random unhelpful page
    
    - Now the message is embedded in the login page

--- index.js
+++ index.js
@@ -56,52 +56,53 @@ app.post('/login', function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-	firebase.auth().signInWithEmailAndPassword(username, password).then(function(){
-		//Success
-		req.session.loggedin = true;
-		req.session.username = username;
-		res.redirect('/');
-	})
-	.catch(function() {
-		//Failed
-		res.send('Incorrect username and/or password!');
-	});
+    firebase.auth().signInWithEmailAndPassword(username, password).then(function () {
+        //Success
+        req.session.loggedin = true;
+        req.session.username = username;
+        res.redirect('/');
+    })
+        .catch(function () {
+            //Failed
+            // res.send('Incorrect username and/or password!');
+            res.redirect('/login?error=incorrect_user');
+        });
 
 });
 
 app.post('/signup', function (req, res) {
     const new_username = req.body.username;
     const new_password = req.body.password;
-	const new_password2 = req.body.password2;
+    const new_password2 = req.body.password2;
 
-	if(new_password == new_password2){
-		firebase.auth().createUserWithEmailAndPassword(new_username, new_password).then(function(){
-			// Success
-			res.send('New account created :) Please login');
-			//res.sendFile(`${__dirname}/views/signIn.html`);
-		})
-		.catch(function(error) {
-			// Failed
-			var errorCode = error.code;
-			var errorMessage = error.message;
-			
-			if (errorCode == 'auth/email-already-in-use') {
-				res.send('An account already exists for this email.');
-			}
-			else if (errorCode == 'auth/invalid-email') {
-				res.send('Invalid email!');
-			}
-			else if (errorCode == 'auth/weak-password') {
-				res.send('Weak password!');
-			}
-			else{
-				res.send('AHHHH SOMETHING WENT WRONG!!');
-			}	
-		});
-	}
-	else{
-		res.send('Passwords do not match!');
-	}
+    if (new_password == new_password2) {
+        firebase.auth().createUserWithEmailAndPassword(new_username, new_password).then(function () {
+            // Success
+            res.send('New account created :) Please login');
+            //res.sendFile(`${__dirname}/views/signIn.html`);
+        })
+            .catch(function (error) {
+                // Failed
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                if (errorCode == 'auth/email-already-in-use') {
+                    res.send('An account already exists for this email.');
+                }
+                else if (errorCode == 'auth/invalid-email') {
+                    res.send('Invalid email!');
+                }
+                else if (errorCode == 'auth/weak-password') {
+                    res.send('Weak password!');
+                }
+                else {
+                    res.send('AHHHH SOMETHING WENT WRONG!!');
+                }
+            });
+    }
+    else {
+        res.send('Passwords do not match!');
+    }
 });
 
 app.listen(port, function () {
