5d53dd58c6093ce44a8d81a5b69e1025ada18c38
ChangjunTang <54614068+ChangjunTang@users.noreply.github.com>
2019-09-25 14:41:56 -0500

    working test register btn

--- JJBP-BOT.js
+++ JJBP-BOT.js
@@ -1,6 +1,11 @@
 // Get the modal
 var modal = document.getElementById('id01');
 
+var mainText = document.getElementById("mainText");
+var submitBtn = document.getElementById("submitBtn");
+function submitClick(){
+	window.alert("working");
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
